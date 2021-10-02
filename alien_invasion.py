0943d742de70b6a2335e9b26b67ea8c487492168
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-10-02 02:50:45 -0400

    Added bullet settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,12 +16,11 @@ class AlienInvasion:
             self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
             self.settings.screen_width = self.screen.get_rect().width
             self.settings.screen_height = self.screen.get_rect().height
-            pygame.display.set_caption("Alien Invasion")
-            self.ship = Ship(self)
         else:
             self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
             pygame.display.set_caption("Alien Invasion")
-            self.ship = Ship(self)
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
--- settings.py
+++ settings.py
@@ -14,3 +14,10 @@ class Settings:
             self.ship_speed = 3.5
         else:
             self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        
