c065c5196f23cee66c658b004daa71fa2dcb73cf
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-10-03 04:40:00 -0400

    Created and instance of the alien.

--- alien.py
+++ alien.py
@@ -10,9 +10,9 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
 
-        # Load the image and set its rect attribute.
+        # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
-        self.rect = self.image.get_rec()
+        self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -76,6 +80,12 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -83,6 +93,7 @@ class AlienInvasion:
 
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
